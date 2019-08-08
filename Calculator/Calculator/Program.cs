711f570af5c2f712ab4de2cac12821144884369a
kapitan21099 <kapitan21099@gmail.com>
2019-08-08 20:50:35 +0300

    add hello world

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,8 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
+            Console.WriteLine();
         }
     }
 }
