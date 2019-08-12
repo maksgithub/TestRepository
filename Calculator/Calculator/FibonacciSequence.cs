73eb85a153b5db62f5fb3c92ebcc21b2fea2410e
marstester <marsvenus777@gmail.com>
2019-08-12 22:36:07 +0300

    Fibonacci sequence methods

---
+++ Calculator/Calculator/FibonacciSequence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class FibonacciSequence
+    {
+        readonly List<int> febSeq;
+        public FibonacciSequence(int length)
+        {
+            febSeq = new List<int>(length);
+        }
+
+
+        public void FibonacciPrint(List<int> febSeq, int length)
+        {
+            Console.WriteLine("Fibonacci sequence from 0 to "+length+":");
+            foreach (int i in febSeq)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public int FibonacciSequenceSum(List<int> febSeq)
+        {
+            int sum = 0;
+            foreach (int i in febSeq)
+            {
+                sum = i + sum;
+            }
+            return sum;
+        }
+
+        public List<int> CreateSequence(int length)
+        {
+            int a = 0;
+            int b = 1;
+            febSeq.Add(a);
+            febSeq.Add(b);
+
+            for (int i = 2; i < length; i++)
+            {
+                int c = a + b;
+                if (c < length)
+                {
+                    febSeq.Insert(i, c);
+                    a = b;
+                    b = c;
+                }
+            }
+            return febSeq;
+        }
+    }
+}
--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,8 +10,17 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            
+            Console.WriteLine("Mars homework!");
+            Console.WriteLine("Enter edge for Fibonacci sequence: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            FibonacciSequence fibonacci = new FibonacciSequence(length);
+            List<int> fibSec = fibonacci.CreateSequence(length);
+            Console.WriteLine("- - - - - - - - - - ");
+            fibonacci.FibonacciPrint(fibSec,length);
+            Console.WriteLine("- - - - - - - - - - ");
+            Console.WriteLine("Fibonacci sequence sum: "+ fibonacci.FibonacciSequenceSum(fibSec));
+            Console.ReadKey();
         }
     }
 }
