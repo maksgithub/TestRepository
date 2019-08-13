a1d3498b7c1f076e449fd2e3532c65b2d0b99a41
dimon <bigderih@gmail.com>
2019-08-13 20:32:58 +0300

    12

---
+++ CommonProjects/CommonProjects/Window Price Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window_Price_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello wrold");
+           
+        }
+      
+    }
+}
