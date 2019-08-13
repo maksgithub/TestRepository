5316d6209dbb24a3e2d54b2e9a515976415b6644
dimon <bigderih@gmail.com>
2019-08-13 20:38:12 +0300

    видалив

--- CommonProjects/CommonProjects/Window Price Calculator/Program.cs
+++ CommonProjects/CommonProjects/Window Price Calculator/Program.cs
@@ -11,8 +11,6 @@ namespace Window_Price_Calculator
         static void Main(string[] args)
         {
             Console.WriteLine(" Hello wrold");
-           
-        }
-      
-    }
+                   }
+          }
 }
