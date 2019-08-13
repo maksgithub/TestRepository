483ec3ef2f9f162954b9bdb055e5d764259d72ae
dimon <bigderih@gmail.com>
2019-08-13 14:43:37 +0300

    пробую учиться шо таке pull request

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,10 +8,48 @@ namespace Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void PrintArray(int[,] arr, int width, int height)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void FillArray(int[,] arr, int width, int height)
+        {
+            int buffer = 0;
+            for (int i = 0; i < width; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        arr[i, j] = buffer++;
+                    }
+                }
+                else
+                {
+                    for (int j = width - 1; j >= 0; j--)
+                    {
+                        arr[i, j] = buffer++;
+                    }
+                }
+            }
+        }
+               static void Main(string[] args)
+        {
+            var width = 5;
+            var height = 5;
+            int arr[i,j] = new int arr [i,j];
+            FillArray(arr, width, height);
+            PrintArray(arr, width, height);
+            
+
+            Console.ReadKey();
         }
     }
 }
