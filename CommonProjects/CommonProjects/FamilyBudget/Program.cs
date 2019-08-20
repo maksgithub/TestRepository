f8d6afb77d16b8189ddc2189bf158970ba1bdcdf
marstester <marsvenus777@gmail.com>
2019-08-20 21:50:09 +0300

    Welcome screen logic #18

--- CommonProjects/CommonProjects/FamilyBudget/Program.cs
+++ CommonProjects/CommonProjects/FamilyBudget/Program.cs
@@ -10,6 +10,42 @@ namespace FamilyBudget
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            while (true)
+            {
+                Console.Clear();
+                OpenMainPage();
+                i++;
+                DetermineKeyAction();
+            }
+        }
+
+        private static void DetermineKeyAction()
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.E:
+                    Environment.Exit(0);
+                    break;
+                case ConsoleKey.A:
+                    OpenAboutPage();
+                    break;
+            }
+        }
+
+        private static void OpenMainPage()
+        {
+            Console.WriteLine("Hello in Family Budget program!");
+            Console.WriteLine("Press A for about");
+            Console.WriteLine("Press E for exit");
+        } 
+
+        private static void OpenAboutPage()
+        {
+            Console.Clear();
+            Console.WriteLine("Project Name version 1.0.0.0");
+            Console.WriteLine("Press any key to return ...");
+            Console.ReadKey();
         }
     }
 }
