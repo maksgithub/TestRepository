0b9e61bed34c891d96463fea786764cfc8a9b8fb
marstester <marsvenus777@gmail.com>
2019-08-22 22:26:37 +0300

    Task 18 improvements

---
+++ CommonProjects/CommonProjects/FamilyBudget/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyBudget
+{
+    class Application
+    {
+        private AppHelper _appHelper; 
+        public Application(AppHelper appHelper)
+        {
+            _appHelper = appHelper;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                _appHelper.OpenMainPage();
+                _appHelper.DetermineKeyAction();
+            }
+        }
+    }
+}
---
+++ CommonProjects/CommonProjects/FamilyBudget/GlobalStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyBudget
+{
+    class GlobalStrings
+    {
+       public static string MainPageHello = "Hello in Family Budget program!";
+       public static string ExitByE = "Press E for exit";
+       public static string AboutByA = "Press A for about";
+
+       public static string AboutPageVersion = "Project Name version 1.0.0.0";
+       public static string ExitByAnyKey = "Press any key to return ...";
+    }
+}
