bfefe349edb30cb9a341387470b2e923cd0575fd
Max <maxim.tatsenko@gmail.com>
2019-08-22 20:29:38 +0300

    Add test Examples

---
+++ CommonProjects/CommonProjects/Empty/Classes/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empty.Classes
+{
+    class Shape
+    {
+        internal double Square { get; set; }
+        public void PrintSquare()
+        {
+            Console.WriteLine($"Square = {Square};");
+        }
+    }
+}
