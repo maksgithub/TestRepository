d18dd9a7cac17eeec88320a54f24678699906447
dimon <bigderih@gmail.com>
2019-08-22 21:01:23 +0300

    refactoring: extract methods

--- CommonProjects/CommonProjects/Window Price Calculator/Program.cs
+++ CommonProjects/CommonProjects/Window Price Calculator/Program.cs
@@ -10,12 +10,14 @@ namespace Window_Price_Calculator
     {
         static void Main(string[] args)
         {
+            RunApplication();
+        }
 
+        private static void RunApplication()
+        {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Press E for exit");
-                Console.WriteLine("Press A for about");
+                PrintMainMenu();
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
                 if (keyInfo.Key == ConsoleKey.E)
                 {
@@ -23,13 +25,24 @@ namespace Window_Price_Calculator
                 }
                 if (keyInfo.Key == ConsoleKey.A)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Project name version 1.0");
-                    Console.WriteLine("Press any key to return...");
-                    Console.ReadKey();
+                    PrintAboutMenu();
                 }
-
             }
+        }
+
+        private static void PrintAboutMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Project name version 1.0");
+            Console.WriteLine("Press any key to return...");
+            Console.ReadKey();
+        }
+
+        static void PrintMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Press E for exit");
+            Console.WriteLine("Press A for about");
         }
     }
 }
