b388890209653cfab318bdf5368b08e109eb6899
Max <maxim.tatsenko@gmail.com>
2019-08-27 19:59:14 +0300

    Add test projects

---
+++ CommonProjects/CommonProjects/Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>(200);
+            List<int> list2 = new List<int>();
+            List<bool> list3 = new List<bool>();
+            List<ConsoleKeyInfo> list4 = new List<ConsoleKeyInfo>();
+
+            list.Add("bla_bla_1");
+            list.Add("2");
+
+            list.AddRange(new[] {"3", "4"});
+            bool isExist = list.Contains("4");
+            list.Remove("bla_bla_1");
+
+            int index = list.IndexOf("3");
+            Console.WriteLine($"index = {index}");
+
+            Console.WriteLine(list[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CommonProjects/CommonProjects/Collections__Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections__Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<int>> dict2 = new Dictionary<string, List<int>>();
+            dict2.Add("Ivan", new List<int>() { 11, 12, 10 });
+
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(1, "Max");
+            dict.Add(0, "Mars");
+            dict.Add(38467, "Vano");
+
+            string name1 = dict[1];
+            Console.WriteLine(name1);
+
+            string name2;
+            bool isExist = dict.TryGetValue(45, out name2);
+            Console.WriteLine(name2);
+
+            string name3;
+            bool isExist2 = dict.TryGetValue(0, out name3);
+            Console.WriteLine(name2);
+
+            dict.TryGetValue(456, out _);
+            Console.WriteLine(name2);
+
+            Console.ReadLine();
+        }
+    }
+}
