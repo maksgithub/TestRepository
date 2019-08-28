1611c25d485442426b87a432717293795bdaa673
kapitan21099 <kapitan21099@gmail.com>
2019-08-29 01:03:10 +0300

    зробив 19 завдання, але з помилками

--- CommonProjects/CommonProjects/RepairCostCalculator/Program.cs
+++ CommonProjects/CommonProjects/RepairCostCalculator/Program.cs
@@ -10,29 +10,70 @@ namespace RepairCostCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press E for exit\nPress A for about");
-            ConsoleKeyInfo k;           
-            k=Console.ReadKey();
-              while (k.Key!=ConsoleKey.E & k.Key!=ConsoleKey.A)                
-                    {Console.Clear();
-                     Console.WriteLine("Press E for exit\nPress A for about");
-                     k=Console.ReadKey();                     
-                    }  
-              if(k.Key==ConsoleKey.A)  
-                                     {
-                                      Console.Clear();
-                                      Console.WriteLine("Project RepairCostCalculator version 1.0.0.0\nPress any key to return ...");
-                                      Console.ReadLine();
-                                      }
-               else
-                   Console.WriteLine();       
+            ConsoleKeyInfo k;
+            while (true)
+            {
+                Console.WriteLine("Press E for exit\nPress A for about\nPress U to create user");
+                k = Console.ReadKey();
+                if (k.Key == ConsoleKey.E)
+                {
+                    break;
+                }
+                if (k.Key == ConsoleKey.A)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Project RepairCostCalculator version 1.0.0.0\nPress any key to return ...");
+                    Console.ReadKey();
+                }
+                if (k.Key == ConsoleKey.U)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write user name and press Enter key");
+                    
+                    User();
+                    Console.ReadLine();
+                }
+                Console.Clear();
+            }
+
         }
+        private static void User()
+        {
+            string name;
+            name = Console.ReadLine();
+            if (CheckString(name) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Only characters are allowed");
+            }
+            int lname = name.Length;
+            if (lname < 3 & lname > 15)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Name must be longer than 3 characters and less than 15 characters");
+            }
+
+
+
+        }
+        static bool CheckString(string name)
+        {
+            for (int i = 0; i < name.Length; ++i)
+            {
+                if (char.IsLetter(name[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+
+
+
+
+
     }
 }
-
-         
-        
-    
-             
-             
-      
