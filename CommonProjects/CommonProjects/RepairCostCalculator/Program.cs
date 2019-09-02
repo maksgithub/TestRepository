1c7f47da6328c629268efa676a55ff77503b9192
kapitan21099 <kapitan21099@gmail.com>
2019-09-02 23:39:23 +0300

    fix19

--- CommonProjects/CommonProjects/RepairCostCalculator/Program.cs
+++ CommonProjects/CommonProjects/RepairCostCalculator/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RepairCostCalculator
 {
     class Program
@@ -15,72 +16,76 @@ namespace RepairCostCalculator
             {
                 Console.WriteLine("Press E for exit\nPress A for about\nPress U to create user");
                 k = Console.ReadKey();
-                if (k.Key == ConsoleKey.E)
+                Console.Clear();
+                switch (k.Key)
                 {
-                    break;
+                    case ConsoleKey.E:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    case ConsoleKey.A:
+                        {                            
+                            Console.WriteLine("Project RepairCostCalculator version 1.0.0.0\nPress any key to return ...");                            
+                            break;
+                        }
+                    case ConsoleKey.U:
+                        {                            
+                            Console.WriteLine("Write user name and press Enter key");
+                            CreateUser();
+                            
+                            break;
+                        }                         
                 }
-                if (k.Key == ConsoleKey.A)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Project RepairCostCalculator version 1.0.0.0\nPress any key to return ...");
-                    Console.ReadKey();
-                }
-                if (k.Key == ConsoleKey.U)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Write user name and press Enter key");
-                    CreateUser();
-                    Console.ReadLine();
-                }
+                Console.ReadKey();
                 Console.Clear();
             }
-        }
-        private static User CreateUser()
-        {
-            string name;
-            name = Console.ReadLine();
-            bool isNotLetter = !IsContainsOnlyLetters(name);
-            if (isNotLetter)
+
+             User CreateUser()
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nOnly characters are allowed");
-
-            }
-
-            int lname = name.Length;
-            bool isNotLength = (lname < 3 || lname > 15);
-            if (isNotLength)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nName must be longer than 3 characters and less than 15 characters");
-
-            }
-
-
-            Console.ForegroundColor = ConsoleColor.White;
-            if (!isNotLength & !isNotLetter)
-            {
-                Console.WriteLine("User created");
-                return new User();
-            }
-            else
-            {
-                Console.WriteLine("User not created");
-                return null;
-            }
-
-        }
-        static bool IsContainsOnlyLetters(string name)
-        {
-            for (int i = 0; i < name.Length; ++i)
-            {
-                char c = name[i];
-                if (!char.IsLetter(c))
+                string name;
+                name = Console.ReadLine();
+                bool isNotLetter = !IsContainsOnlyLetters(name);
+                if (isNotLetter)
                 {
-                    return false;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nOnly characters are allowed");
                 }
+
+                int lname = name.Length;
+                bool isNotLength = (lname < 3 || lname > 15);
+                if (isNotLength)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nName must be longer than 3 characters and less than 15 characters");
+                }
+
+
+                Console.ForegroundColor = ConsoleColor.White;
+                if (!isNotLength & !isNotLetter)
+                {
+                    Console.WriteLine("User created");
+                    return new User();
+                }
+                else
+                {
+                    Console.WriteLine("User not created");
+                    return null;
+                }
+
             }
-            return true;
+            bool IsContainsOnlyLetters(string name)
+            {
+                for (int i = 0; i < name.Length; ++i)
+                {
+                    char c = name[i];
+                    if (!char.IsLetter(c))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
