167dd8a8c63297177a0488a116028b7eab47d640
Max <maxim.tatsenko@gmail.com>
2019-09-12 18:48:55 +0300

    Add test projects

---
+++ CommonProjects/CommonProjects/Static_Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Static_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person max = new Person("Max", 26);
+            max.Jump();
+            Console.WriteLine(max.Calculate(56));
+
+            Person ivan = new Person("Max", 26);
+            ivan.Jump();
+            int a = ivan.Calculate(44);
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
