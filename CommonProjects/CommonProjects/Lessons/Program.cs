71da57706d7316ef9faa300d79e0067f00c62dcc
Maks <maxim.tatsenko@gmail.com>
2019-09-26 19:31:56 +0300

    Add projects

---
+++ CommonProjects/CommonProjects/Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
