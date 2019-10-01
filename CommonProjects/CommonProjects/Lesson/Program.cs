9dbfb73a2e83a9e7eb9368adab507161e5396ef2
kapitan21099 <kapitan21099@gmail.com>
2019-10-01 19:49:14 +0300

    Slon

--- CommonProjects/CommonProjects/Lesson/Program.cs
+++ CommonProjects/CommonProjects/Lesson/Program.cs
@@ -10,6 +10,14 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
+            Slon slon1 = new Slon();
+            slon1.Rost = 59;
+
+            slon1.PrintAll();           
+
+            Console.ReadLine();
+            
         }
+       
     }
 }
---
+++ CommonProjects/CommonProjects/Lesson/Slon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class Slon
+    {
+        public int Wes { get; }
+        public int Rost { private get; set; }
+        private string Name { get; set; }
+
+        private const int countLegs = 4;
+        public const string father = "Dimon";
+        public string _chastTela;
+        private int _vozrast;
+
+        public  Slon()
+        {
+           
+        }
+        
+         public void PrintAll()
+        {
+            Console.WriteLine(Name+" "+Calculate());
+
+
+        }
+         public int Calculate()
+        {
+            return Wes * Wes;
+        }
+    }
+}
